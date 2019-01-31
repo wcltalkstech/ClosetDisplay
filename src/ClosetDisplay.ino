623893e5338f197f3fd1e1adf9ce2e9ffc31a2bd
Kevin McQuown <kevin@glancemirror.com>
2019-01-31 10:57:48 -0600

    just on mesh network listening and displaying time from last update

--- src/ClosetDisplay.ino
+++ src/ClosetDisplay.ino
@@ -1,25 +1,47 @@
+SYSTEM_THREAD(ENABLED);
+SYSTEM_MODE(MANUAL);
+
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
 
 #define OLED_RESET D4
 Adafruit_SSD1306 display(OLED_RESET);
 
+unsigned long timeOfLastUpdate;
+unsigned long delta;
+char timeString[10];
+
+void displayMessage(uint8_t textSize, uint8_t lineNumber, const char *data) {
+  display.setTextSize(textSize);
+  display.setTextColor(WHITE);
+  display.setCursor(0,lineNumber*18);
+  display.println(data);
+  display.display();
+}
+
 void showTemp(const char *event, const char *data)
 {
+  delta = millis() - timeOfLastUpdate;
+  timeOfLastUpdate = millis();
   display.clearDisplay();
-  display.setCursor(0,5);
-  display.print(data);
-  display.display();
+  displayMessage(2, 0, data);
+  snprintf(timeString, sizeof(timeString), "%d", delta);
+  displayMessage(2, 1, timeString);
 }
 
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  timeOfLastUpdate = millis();
+
   display.clearDisplay();
-  display.setTextSize(3);
-  display.setTextColor(WHITE);
-  display.setCursor(0,5);
-  display.println("WAITING");
-  display.display();
+  displayMessage(2, 0, "MESH...");
+  Mesh.on();
+  Mesh.connect();
+  waitUntil(Mesh.ready);
+
+  display.clearDisplay();
+  displayMessage(2, 0, "WAITING");
 
   Mesh.subscribe("newTemp", showTemp);
 }
